15c1403649747eccc6ca408fc12ab70eb7a8263a
Aaron Shafovaloff <aaronshaf@gmail.com>
2017-02-15 22:52:10 -0700

    initial commit for validating responses

---
+++ examples/validate-responses/index.js
@@ -0,0 +1,26 @@
+const confident = require('../../index') // require('confident')
+const express = require('express')
+const app = express()
+const path = require('path')
+
+function greet (req, res) {
+  res.json('Hello, world.')
+}
+
+app.use(confident({
+  specification: path.join(__dirname, './api.yml'),
+  docsEndpoint: '/docs',
+  operations: { greet },
+  onResponseValidationError: (req, res, errors, next) => {
+    console.log(errors)
+    next()
+  }
+}))
+
+if (!module.parent) {
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!')
+  })
+}
+
+module.exports = app
